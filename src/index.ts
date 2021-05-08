aeb37e2fe80d82848cec672bd9d4719ab34afc2c
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-07 21:46:02 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { noop } from './lib/noop'
+export { getItemCyclic } from './lib/getItemCyclic'
+export { getRandomItem } from './lib/getRandomItem'
+export { patternChunks } from './lib/patternChunks'
+export { replaceAt } from './lib/replaceAt'
