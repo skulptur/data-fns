e9adf6cd8c2cc021914b258639045d361cd46ed2
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-23 21:34:52 -0300

    Update getRandomItem.ts

--- src/lib/getRandomItem.ts
+++ src/lib/getRandomItem.ts
@@ -1,3 +1,5 @@
-export const getRandomItem = <T>(items: Array<T>): T => {
-  return items[Math.floor(Math.random() * items.length)]
+import { toIndex } from 'unit-fns'
+
+export const getRandomItem = <T>(items: Array<T>, random = Math.random): T => {
+  return items[toIndex(random(), items.length)]
 }
