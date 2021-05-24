4649e0499e019806fc31db42afb70e498b734e79
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-23 21:34:58 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { getItemCyclic } from './lib/getItemCyclic'
 export { getRandomItem } from './lib/getRandomItem'
+export { offset } from './lib/offset'
 export { patternChunks } from './lib/patternChunks'
 export { replaceAt } from './lib/replaceAt'
+export { times } from './lib/times'
