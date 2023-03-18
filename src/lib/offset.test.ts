8b4d9d89dd5f18cfc6c13ea9cd05e8c0b04fbd0f
skulptur <g.spredemann@gmail.com>
2023-03-18 22:57:47 +0100

    Create offset.test.ts

---
+++ src/lib/offset.test.ts
@@ -0,0 +1,30 @@
+import { offset } from './offset'
+
+describe('offset', () => {
+  it('should return the same array if the offset amount is zero', () => {
+    const input = [1, 2, 3, 4, 5]
+    const output = offset(input, 0)
+    expect(output).toEqual(input)
+  })
+
+  it('should return a new array with elements offset by the given amount', () => {
+    const input = [1, 2, 3, 4, 5]
+    const offsetAmount = 2
+    const output = offset(input, offsetAmount)
+    expect(output).toEqual([3, 4, 5, 1, 2])
+  })
+
+  it('should handle negative offset amounts correctly', () => {
+    const input = [1, 2, 3, 4, 5]
+    const offsetAmount = -2
+    const output = offset(input, offsetAmount)
+    expect(output).toEqual([4, 5, 1, 2, 3])
+  })
+
+  it('should not modify the original array', () => {
+    const input = [1, 2, 3, 4, 5]
+    const offsetAmount = 2
+    offset(input, offsetAmount)
+    expect(input).toEqual([1, 2, 3, 4, 5])
+  })
+})
