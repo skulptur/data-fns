77d31bd4db6889561ce6ff1ebb073194f3cf0ddf
skulptur <g.spredemann@gmail.com>
2023-03-18 23:31:13 +0100

    add tests and fix palindrome

--- src/lib/getItemPalindrome.ts
+++ src/lib/getItemPalindrome.ts
@@ -1,10 +1,15 @@
 export const getItemPalindrome = <T>(index: number, array: Array<T>): T => {
-    const arraySize = array.length
-    const id = index % (2 * arraySize - 2)
+  const arraySize = array.length
   
-    if (id < arraySize) {
-      return array[id]
-    } else {
-      return array[2 * arraySize - id - 2]
-    }
+  if (arraySize === 1) {
+    return array[0]
+  }
+
+  const id = index % (2 * arraySize - 2)
+
+  if (id < arraySize) {
+    return array[id]
+  } else {
+    return array[2 * arraySize - id - 2]
+  }
 }
