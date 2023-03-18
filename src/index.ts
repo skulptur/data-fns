2d4e1fbce0e7018f519787691d4a2550342844ba
skulptur <g.spredemann@gmail.com>
2023-03-18 21:32:22 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export { getItemReverseCyclic } from './lib/getItemReverseCyclic'
+export { getItemPalindrome } from './lib/getItemPalindrome'
 export { getItemCyclic } from './lib/getItemCyclic'
 export { offset } from './lib/offset'
 export { patternChunks } from './lib/patternChunks'
